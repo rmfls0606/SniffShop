3d249f32847fee27049464d77eec81edbc2f9031
rmfls0606 <rmfls0606@naver.com>
2025-07-26 01:20:36 +0900

    Feat - 상품 검색을 위한 mainSearchBar View 구성 및 AutoLayout 설정

--- SniffShop/MainViewController.swift
+++ SniffShop/MainViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: UIViewController {
-
+    
+    //MARK: - View
+    private let mainSearchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.tintColor = .systemGray3
+        searchBar.barTintColor = .black
+        searchBar.searchTextField.textColor = .systemGray3
+        searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "브랜드, 상품, 프로필, 태그 등",
+                                                                             attributes: [.foregroundColor: UIColor.systemGray3])
+        searchBar.searchTextField.leftView?.tintColor = .systemGray3
+        return searchBar
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
     }
-
-
 }
 
+extension MainViewController: ViewDesignProtocol{
+    func configureHierarchy() {
+        view.addSubview(mainSearchBar)
+    }
+    
+    func configureLayout() {
+        mainSearchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
+    }
+    
+    func configureView() {
+        view.backgroundColor = .black
+        
+        navigationItem.title = "킁킁쇼핑몰"
+    }
+}

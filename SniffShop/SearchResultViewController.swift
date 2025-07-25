66f8974bec986f8455ed9ea917a1be91986cffda
rmfls0606 <rmfls0606@naver.com>
2025-07-26 02:25:20 +0900

    Add - 검색 결과 화면 SearchResultViewController.swift

---
+++ SniffShop/SearchResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SearchResultViewController.swift
+//  SniffShop
+//
+//  Created by 이상민 on 7/26/25.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+}
+
+extension SearchResultViewController: ViewDesignProtocol{
+    func configureHierarchy() {
+        
+    }
+    
+    func configureLayout() {
+        
+    }
+    
+    func configureView() {
+        
+    }
+    
+    
+}

cc2e23bb0c80b2784dc13694e5cdd77a9d555e0d
rmfls0606 <rmfls0606@naver.com>
2025-07-26 02:15:58 +0900

    Feat - 모든 화면에서 navigationItem.title의 색은 white로 설정

--- SniffShop/SceneDelegate.swift
+++ SniffShop/SceneDelegate.swift
@@ -17,6 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

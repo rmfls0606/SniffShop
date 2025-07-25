1163d9aa8b4cf5e08ca2c4a5b689fecc74a51636
rmfls0606 <rmfls0606@naver.com>
2025-07-26 00:34:51 +0900

    Add - 뷰 설정을 분리하기 위한 ViewDesignProtocol.swfit

---
+++ SniffShop/ViewDesignProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ViewDesignProtocol.swift
+//  SniffShop
+//
+//  Created by 이상민 on 7/26/25.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol{
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+}

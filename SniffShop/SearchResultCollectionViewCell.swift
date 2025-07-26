2ffae526a86930698804527f37a6310dddaa478a
rmfls0606 <rmfls0606@naver.com>
2025-07-27 02:59:36 +0900

    Feat - 검색 결과 collectionView와 연결하기 위한 identifier 설정

--- SniffShop/SearchResultCollectionViewCell.swift
+++ SniffShop/SearchResultCollectionViewCell.swift
@@ -9,4 +9,32 @@ import UIKit
 
 class SearchResultCollectionViewCell: UICollectionViewCell {
     
+    //MARK: - Property
+    static let identifier = "SearchResultCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension SearchResultCollectionViewCell: ViewDesignProtocol{
+    func configureHierarchy() {
+       
+    }
+    
+    func configureLayout() {
+      
+    }
+    
+    func configureView() {
+        contentView.backgroundColor = .orange
+    }
 }

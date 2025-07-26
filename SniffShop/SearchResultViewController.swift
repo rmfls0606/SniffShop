b2fa259498c7f9b3a8e1d7e2ad34d9e57f245d8d
rmfls0606 <rmfls0606@naver.com>
2025-07-26 13:01:54 +0900

    Feat - 검색 결과 개수 보여주는 Label 구성 및 AutoLayout 설정

--- SniffShop/SearchResultViewController.swift
+++ SniffShop/SearchResultViewController.swift
@@ -6,8 +6,17 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchResultViewController: UIViewController {
+    
+    private let resultCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "13,235,449 개의 검색 결과"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .green
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,15 +29,20 @@ class SearchResultViewController: UIViewController {
 
 extension SearchResultViewController: ViewDesignProtocol{
     func configureHierarchy() {
-        
+        view.addSubview(resultCountLabel)
     }
     
     func configureLayout() {
-        
+        resultCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
+        }
     }
     
     func configureView() {
+        view.backgroundColor = .black
         
+        navigationItem.title = "킁킁인형"
     }
     
     

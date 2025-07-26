96d41b79de390e20abd7b28e4848fb9caea429d1
rmfls0606 <rmfls0606@naver.com>
2025-07-27 02:29:17 +0900

    Feat - 검색 결과 목록들을 보여주기 위한 collectionView 생성 및 AutoLayout 설정

--- SniffShop/SearchResultViewController.swift
+++ SniffShop/SearchResultViewController.swift
@@ -10,12 +10,33 @@ import SnapKit
 
 class SearchResultViewController: UIViewController {
     
+    //MARK: - View
     private let resultCountLabel: UILabel = {
         let label = UILabel()
         label.text = "13,235,449 개의 검색 결과"
         label.font = .systemFont(ofSize: 12)
         label.textColor = .green
         return label
+    }()
+    
+    private let resultCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        let width = UIScreen.main.bounds.width
+        let padding = 16.0
+        let spacing = 16.0
+        let itemCount = 2.0
+        
+        let dimension = (width - (padding * 2) - (spacing * (itemCount - 1))) / itemCount
+        layout.itemSize = CGSize(width: dimension, height: dimension * 1.4)
+        layout.minimumInteritemSpacing = 16
+        layout.minimumLineSpacing = 16
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 16, bottom: 16, right: 16)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        
+        return collectionView
     }()
 
     override func viewDidLoad() {
@@ -30,6 +51,8 @@ class SearchResultViewController: UIViewController {
 extension SearchResultViewController: ViewDesignProtocol{
     func configureHierarchy() {
         view.addSubview(resultCountLabel)
+        
+        view.addSubview(resultCollectionView)
     }
     
     func configureLayout() {
@@ -37,13 +60,33 @@ extension SearchResultViewController: ViewDesignProtocol{
             make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(16)
         }
+        
+        resultCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(resultCountLabel.snp.bottom).offset(8)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
     }
     
     func configureView() {
         view.backgroundColor = .black
         
         navigationItem.title = "킁킁인형"
+        
+        resultCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        resultCollectionView.delegate = self
+        resultCollectionView.dataSource = self
     }
-    
-    
+}
+
+extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .gray
+        
+        return cell
+    }
 }

6d8d80a9b1fbed1488c3c410f577ed2e2519d7d9
rmfls0606 <rmfls0606@naver.com>
2025-07-27 02:33:13 +0900

    Add - 검색 결과 화면 아이템 SearchResultCollectionViewCell.swift

---
+++ SniffShop/SearchResultCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResultCollectionViewCell.swift
+//  SniffShop
+//
+//  Created by 이상민 on 7/27/25.
+//
+
+import UIKit
+
+class SearchResultCollectionViewCell: UICollectionViewCell {
+    
+}

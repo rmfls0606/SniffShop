7462e53811067edd1fe39fe4f3f301c11a0bd151
rmfls0606 <rmfls0606@naver.com>
2025-07-30 05:27:20 +0900

    Feat - 검색 결과 광고 화면 Item Cell에 상품 이미지 View 구성 및 AutoLayout 설정

--- SniffShop/View/SearchResult/ADCollectionViewCell.swift
+++ SniffShop/View/SearchResult/ADCollectionViewCell.swift
@@ -6,8 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ADCollectionViewCell: BaseCollectionViewCell {
     
     static let identifier = "ADCollectionViewCell"
+    
+    private let adImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .systemGray6
+        return imageView
+    }()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(adImageView)
+    }
+    
+    override func configureLayout() {
+        adImageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        
+    }
 }

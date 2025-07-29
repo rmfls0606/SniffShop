d8147569d48d5b2b812e8187846df8bbf6b74491
rmfls0606 <rmfls0606@naver.com>
2025-07-30 03:19:20 +0900

    Feat - showAlert에 취소 버튼 추가 여부 매개변수 추가

--- SniffShop/Extensions/UIViewController+Extension.swift
+++ SniffShop/Extensions/UIViewController+Extension.swift
@@ -8,14 +8,18 @@
 import UIKit
 
 extension UIViewController{
-    func showAlert(title: String?, message: String?, checkButtonTitle: String , completion: @escaping () -> Void){
+    func showAlert(title: String?, message: String?, checkButtonTitle: String, isCancelButton: Bool = false, completion: @escaping () -> Void){
         let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        let checkAction = UIAlertAction(title: checkButtonTitle, style: .default) { _ in
+        let checkAction = UIAlertAction(title: checkButtonTitle, style: isCancelButton ? .destructive : .default) { _ in
             completion()
         }
-        
         alertVC.addAction(checkAction)
+        
+        if isCancelButton{
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+            alertVC.addAction(cancelAction)
+        }
         
         present(alertVC, animated: true)
     }

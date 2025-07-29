f1926264b638f5d5c7c2d538ad53643e92f5d11f
rmfls0606 <rmfls0606@naver.com>
2025-07-30 01:06:02 +0900

    Feat - 네트워크 통신 로직 싱글톤 패턴으로 구현

---
+++ SniffShop/Manager/NetworkManager.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkManager.swift
+//  SniffShop
+//
+//  Created by 이상민 on 7/29/25.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager{
+    static let shared = NetworkManager()
+    
+    private init() { }
+    
+    func callRequest<T: Decodable>(api: NaverRequest,
+                                   successHandler: @escaping (T)  -> Void,
+                                   failureHandler: @escaping (Error) -> Void){
+        AF.request(api.endPoint,
+                   method: api.method,
+                   parameters: api.parameters,
+                   headers: api.headers)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: T.self) { response in
+                switch response.result{
+                case .success(let value):
+                    successHandler(value)
+                case .failure(let error):
+                    failureHandler(error)
+                }
+            }
+    }
+}

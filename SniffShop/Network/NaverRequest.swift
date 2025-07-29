d62cd98eff88d328f37270b8029e9a6518617932
rmfls0606 <rmfls0606@naver.com>
2025-07-30 01:04:58 +0900

    Feat - 쇼핑 API 요청을 위한 enum 구조 정의

---
+++ SniffShop/Network/NaverRequest.swift
@@ -0,0 +1,47 @@
+//
+//  NaverRequest.swift
+//  SniffShop
+//
+//  Created by 이상민 on 7/29/25.
+//
+
+import Foundation
+import Alamofire
+
+enum NaverRequest{
+    case shopping(query: String, sort: String, start: Int)
+    
+    var baseURL: String{
+        NetworkURL.naverShopping
+    }
+    
+    var path: String{
+        switch self{
+        case .shopping:
+            return "/v1/search/shop.json"
+        }
+    }
+    
+    var parameters: [String: Any]{
+        switch self{
+        case .shopping(let query, let sort, let start):
+            ["query": query, "sort": sort, "start": start]
+        }
+    }
+    
+    var endPoint: String{
+        switch self{
+        case .shopping:
+            return baseURL + path
+        }
+    }
+    
+    var method: HTTPMethod{
+        return .get
+    }
+    
+    var headers: HTTPHeaders{
+        return ["X-Naver-Client-Id": APIKeyManager.naverClientID,
+                "X-Naver-Client-Secret": APIKeyManager.naverClienSecret]
+    }
+}
---
+++ SniffShop/Network/README.md
@@ -0,0 +1,5 @@
+# Network 폴더
+
+이폴더에는 다음 파일들을 직접 생성해야 합니다.
+
+- NetworkURL.swift (API 주소)

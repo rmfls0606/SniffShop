b32ac3d19d45697c2ab7e15aaca741e311ec20d7
rmfls0606 <rmfls0606@naver.com>
2025-08-12 16:37:50 +0900

    feat: MVVM패턴을 사용하기 위해 Observable 생성

---
+++ SniffShop/Model/Observable.swift
@@ -0,0 +1,32 @@
+//
+//  Observable.swift
+//  SniffShop
+//
+//  Created by 이상민 on 8/12/25.
+//
+
+import Foundation
+
+class Observable<T>{
+    
+    var closure: ((T) -> Void)?
+    
+    var value: T{
+        didSet{
+            closure?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(closure: @escaping((T) -> Void)){
+        closure(value)
+        self.closure = closure
+    }
+    
+    func lazyBind(closure: @escaping((T) -> Void)){
+        self.closure = closure
+    }
+}
